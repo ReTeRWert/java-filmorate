48cca8c7755c55b5ea5f515f71b8c4061890cd2d
84R5 <o.u.n@yandex.ru>
2023-04-01 23:07:24 +0300

    <style> delete import

--- src/main/java/ru/yandex/practicum/filmorate/dao/UserMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/UserMapper.java
@@ -1,11 +1,9 @@
 package ru.yandex.practicum.filmorate.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
